aa5527f3d90a0a02861983ad93cc2b1e8b0f84e6
cjayasur <charitha.jayasuriya@gmail.com>
2020-10-24 19:51:56 -0700

    push from mac

--- Screens/FlexGridScreen.js
+++ Screens/FlexGridScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import { View, StatusBar } from "react-native";
 import  styles from "../CustomProperties/FlexGridStyles";
 import Box from "../Components/Box";
-import {greenA400, yellow800} from "react-native-paper/src/styles/colors";
+import {greenA400, red600, yellow800} from "react-native-paper/src/styles/colors";
 
 const boxes = new Array(10).fill(null).map((v, i) => i + 1);
 
@@ -12,7 +12,7 @@ const FlexGrid = () => {
             <StatusBar hidden={false} />
             {console.log("FlexGrid Console Log")}
             {boxes.map(i => (
-                <Box key={i} boxColor={greenA400} styleObj={styles.box}>#{i}</Box>
+                <Box key={i} boxColor={red600} styleObj={styles.box}>#{i}</Box>
             ))}
         </View>
     )
